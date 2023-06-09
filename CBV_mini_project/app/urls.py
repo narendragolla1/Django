90c717e18f66530d7e7d8d2b8527d979074c8046
narendragolla1 <116960868+narendragolla1@users.noreply.github.com>
2023-06-09 19:18:34 +0530

    CBV

--- CBV_mini_project/app/urls.py
+++ CBV_mini_project/app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.CompanyListView.as_view())
+    path('',views.CompanyListView.as_view()),
+    path('app/<int:id>',views.index),
 ]
--- CBV_mini_project/app/views.py
+++ CBV_mini_project/app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 from app.models import Company
 from django.urls import reverse_lazy
 from django.views.generic import (
@@ -31,6 +31,18 @@ class CompanyDeleteView(DeleteView):
     model=Company
     template_name='company_delete.html'
     success_url=reverse_lazy('companies')
+    
+
+
+def index(request,id):
+    data = [
+        {'id': 10, 'name': 'chaitanya', 'location': 'epuru'},
+        {'id': 11, 'name': 'narayana', 'location': 'eluru'},
+        {'id': 12, 'name': 'narayana', 'location': 'eluru'}
+    ]
+
+    context = {'data': data}
+    return render(request, 'details.html', context)
     
     
 
